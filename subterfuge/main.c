1d43d006ebe6d23993adc1b93cb2218d1ff4497a
Dakota Schneider <dakota@hackthetruth.org>
2016-12-06 23:45:08 -0800

    Initial Commit

---
+++ subterfuge/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  subterfuge
+//
+//  Created by Dakota Schneider on 12/6/16.
+//  Copyright © 2016 Dakota Schneider. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
