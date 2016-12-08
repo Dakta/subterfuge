d09086f7aa1026d6b5679a4c0bbad887e65a1605
Dakota Schneider <dakota@hackthetruth.org>
2016-12-07 20:41:54 -0800

    Cleanup userdata

--- subterfuge/types.h
+++ subterfuge/types.h
@@ -14,20 +14,20 @@
 //struct Type;
 //struct TypeVal;
 //
-typedef struct Type Type;
-typedef struct TypeVal TypeVal;
+//typedef struct Type Type;
+//typedef struct TypeVal TypeVal;
 
-struct Type {
+typedef struct Type {
     char * name;
     size_t size;
-};
+} Type;
 
-struct TypeVal {
+typedef struct TypeVal {
     struct Type *type;
 
     void * val;
     size_t size;
-};
+} TypeVal;
 
 
 Type * Type_new(
@@ -41,25 +41,35 @@ TypeVal * TypeVal_new(Type * type);
 TypeVal * TypeVal_copy(TypeVal * old);
 void TypeVal_free(TypeVal * val);
 
-
+//
 // numeric types
+//
+
+// Integer
 extern Type * Int;
 TypeVal * Int_new();
 intmax_t Int_set(TypeVal * data, intmax_t val);
 intmax_t Int_get(TypeVal * data);
 
+// Floating point
 extern Type * Double;
 TypeVal * Double_new();
 long double Double_set(TypeVal * data, long double val);
 long double Double_get(TypeVal * data);
 
+//
 // string types
+//
+
+// String
 extern Type * String;
 TypeVal * String_new();
 char* String_set(TypeVal * data, char* val);
 char* String_get(TypeVal * data);
 
 
+
+// Rock'n'Roll
 void Types_init();
 
 
